45b3e2c17007966b573ef61e688c02a4f7638164
dhineshrozario@gmail.com <dhineshrozario@dhineshs-mbp.home>
2020-05-26 15:52:39 +0100

    updated the test case with pasue and expect

--- webdriverio/tests/example.js
+++ webdriverio/tests/example.js
@@ -2,5 +2,18 @@ describe ('First Steps with WebdriverIO', () => {
     it('Load Example website', () => {
         browser.url('http://www.example.com')
         browser.pause(2000)
+
+        expect(browser).toHaveUrl('http://www.example.com/')
     })
+
+    it ('Load WebdriverIO Website', ()=> {
+        browser.url('https://webdriver.io')
+        browser.pause(3000)
+    })
+    
+    
+    
+    // it ('', ()=> {
+        
+    // })
 })
